4f6b55b802742b1401b8a60751bc37e2f7b48adf
Abhishek777777777 <abhishekmishra992016@gmail.com>
2023-06-20 21:19:08 +0530

    InWeatherNewCityGetAdded

--- weather.js
+++ weather.js
@@ -1,26 +1,3 @@
-// async function weatherApi() {
-// 	const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi';
-// 	const options = {
-// 		method: 'GET',
-// 		headers: {
-// 			'X-RapidAPI-Key': 'bd00fa80b8msh3771382c2c3c79ep1b3e74jsn63c6f7ae0a3c',
-// 			'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-// 		}
-// 	};
-
-// 	try {
-// 		const response = await fetch(url, options);
-// 		const result = await response.text();
-
-// 	} catch (error) {
-// 		console.error(error);
-// 	}
-// }
-
-// weatherApi()
-
-// Getting Element by Id  
-
 let temp = document.getElementById('temp');
 let temp2 = document.getElementById('temp2');
 let feels_like = document.getElementById('feels_like');
@@ -35,7 +12,15 @@ let sunrise = document.getElementById('sunrise');
 let sunset = document.getElementById('sunset');
 let cityName = document.getElementById('cityName')
 
+
+const rowIndia = document.getElementById('row-india');
+const rowAmerica = document.getElementById('row-america');
 const rowLucknow = document.getElementById('row-lucknow');
+const rowDelhi = document.getElementById('row-delhi');
+const rowBhadohi = document.getElementById('row-bhadohi');
+const rowVaranasi = document.getElementById('row-varanasi');
+const rowAgra = document.getElementById('row-agra');
+
 
 
 var options = {
@@ -46,36 +31,14 @@ var options = {
 	}
 };
 
-// const getWeather = (city) => {
 
-// 	fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=` + city, options)
-// 		.then(response => response.json())
-// 		.then((response) => {
-// 			// cloud_pct.innerHTML = response.cloud_pct
-
-// 			cityName.innerHTML = city
-// 			temp.innerHTML = response.temp
-// 			temp2.innerHTML = response.temp
-// 			feels_like.innerHTML = response.feels_like
-// 			humidity.innerHTML = response.humidity
-// 			humidity2.innerHTML = response.humidity
-// 			min_temp.innerHTML = response.min_temp
-// 			max_temp.innerHTML = response.max_temp
-// 			wind_speed.innerHTML = response.wind_speed
-// 			wind_degrees.innerHTML = response.wind_degrees
-// 			wind_speed2.innerHTML = response.wind_speed
-// 			sunrise.innerHTML = response.sunrise
-// 			sunset.innerHTML = response.sunset
-// 		})
-// 		.catch(err => console.log(err))
-
-// }
 
 async function getRapidApiResponse(city) {
 	try {
 		const response = await fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`, options)
 		console.log(response + city)
-		return await response.json();
+		const data = await response.json();
+		return data;
 	}
 	catch (error) {
 		console.log(error)
@@ -86,7 +49,6 @@ async function getRapidApiResponse(city) {
 
 async function getAndSetCityWather(element, cityName) {
 	const data = await getRapidApiResponse(cityName);
-	console.log(data);
 	element.innerHTML = `
 	<td>${cityName}</td>
 	<td>${data.cloud_pct}</td>
@@ -103,27 +65,23 @@ async function getAndSetCityWather(element, cityName) {
 }
 
 
-
-
-
 // UttarPradesh
 
 const getWeather = async (city) => {
-	let bhadoiData = await getRapidApiResponse(city);
+	let bhadohiData = await getRapidApiResponse(city);
 	cityName.innerHTML = city;
-	temp.innerHTML = bhadoiData.temp;
-	temp2.innerHTML = bhadoiData.temp;
-	feels_like.innerHTML = bhadoiData.feels_like
-	humidity.innerHTML = bhadoiData.humidity
-	humidity2.innerHTML = bhadoiData.humidity
-	min_temp.innerHTML = bhadoiData.min_temp
-	max_temp.innerHTML = bhadoiData.max_temp
-	console.log(bhadoiData.max_temp);
-	wind_speed.innerHTML = bhadoiData.wind_speed
-	wind_degrees.innerHTML = bhadoiData.wind_degrees
-	wind_speed2.innerHTML = bhadoiData.wind_speed
-	sunrise.innerHTML = bhadoiData.sunrise
-	sunset.innerHTML = bhadoiData.sunset
+	temp.innerHTML = bhadohiData.temp;
+	temp2.innerHTML = bhadohiData.temp;
+	feels_like.innerHTML = bhadohiData.feels_like
+	humidity.innerHTML = bhadohiData.humidity
+	humidity2.innerHTML = bhadohiData.humidity
+	min_temp.innerHTML = bhadohiData.min_temp
+	max_temp.innerHTML = bhadohiData.max_temp
+	wind_speed.innerHTML = bhadohiData.wind_speed
+	wind_degrees.innerHTML = bhadohiData.wind_degrees
+	wind_speed2.innerHTML = bhadohiData.wind_speed
+	sunrise.innerHTML = bhadohiData.sunrise
+	sunset.innerHTML = bhadohiData.sunset
 };
 getWeather("Bhadohi");
 
@@ -133,10 +91,15 @@ getWeather("Bhadohi");
 submit.addEventListener("click", (event) => {
 	event.preventDefault();
 	getWeather(city.value)
-	console.log(city.value)
 })
 
+getAndSetCityWather(rowIndia, "India");
 getAndSetCityWather(rowLucknow, "Lucknow");
+getAndSetCityWather(rowBhadohi, "Bhadohi");
+getAndSetCityWather(rowAmerica, "America");
+getAndSetCityWather(rowDelhi, "Delhi");
+getAndSetCityWather(rowVaranasi, "varanasi");
+getAndSetCityWather(rowAgra, "Agra");
 
 
 
